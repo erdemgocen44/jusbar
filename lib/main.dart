d6f5b14ffe4a3dd534a103e27b785311cbe2602d
erdemgocen44 <erdemgocen44@gmail.com>
2021-04-21 11:02:15 +0300

    ilk

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:jusbar/screens/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
